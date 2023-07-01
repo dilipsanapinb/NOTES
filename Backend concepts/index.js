18fc40d31e870c2390ec70d2547f6e8a5d9d9994
dilipsanapinb <dilipinbsanap@gmail.com>
2023-07-01 12:59:24 +0530

    backend

--- Backend concepts/index.js
+++ Backend concepts/index.js
@@ -1,33 +1,13 @@
-
-
-// const express = require('express');
-
-// const app = express();
-
-// const logError = (err,req, res, next) => {
-//     if (req.url !== "/" || req.url !== "/users" || req.url !== "/users.userId") {
-//         res.status(404).send("rout is not matching")
-//     }
-//     next()
-// }
+function func() {
     
-// app.use(logError);
-
-// app.get('/', (req, res) => {
-//     res.send("root path")
-// });
-
-// app.get('/users', () => {
-//     res.send("users data")
-// });
-
-// app.get('/user/:userId', (req,res) => {
-//     let id = req.params.userId;
-// })
-
-// app.listen(8000, () => {
-//     console.log('Server is listening to port 8000');
-// })
+    console.log(x);
+    var x = 10;
+    var abc=()=> {
+        console.log(x)
+    }
+abc();
+}
+func()
 
 
 
